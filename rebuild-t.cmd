dd33517c262609304c545d2efc57e4ba61a01973
Alex Meijer <asure007@gmail.com>
2026-05-25 00:02:58 +0200

    Fix T-Unit build. Add DCS fixes.

--- rebuild-t.cmd
+++ rebuild-t.cmd
@@ -5,9 +5,9 @@ del sys.equ
 del gsp.equ
 copy auditT.equ	audit.equ
 copy dispT.equ disp.equ
+copy linkT.equ link.equ
 copy sysT.equ sys.equ
 copy gspT.equ gsp.equ
-copy linkT.equ link.equ
 c:\dosbox-x\dosbox-x.exe -conf .\dosbox-x\dosbox-x.conf
 del mc*.*
 del *.lck
